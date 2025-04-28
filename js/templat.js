360a08de73b184b03193cf099f545f9411690394
ekerzazi <gkerzazi@gmail.com>
2025-04-28 14:43:55 +0100

    create login page

---
+++ js/templat.js
@@ -0,0 +1,68 @@
+const Login = `
+
+    <div id="loginPage" class="container1">
+    <h2>Login</h2>
+    <form id="loginForm">
+        <input type="text" id="email" placeholder="Email" required>
+        <input type="password" id="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+        <p id="loginError" style="color: red; margin-top: 10px;"></p>
+    </form>
+    </div>
+   
+`
+
+const Profile = `
+
+     <header>
+        <div class="container">
+            <div class="username-box">
+                <h1>Welcome, Mohammed Mihit</h1>
+            </div>
+
+            <div class="logout-section">
+                <button>Logout</button>
+            </div>
+
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="general-info">
+                <h3 style="font-weight: 800; text-align: center;">General Informations</h3>
+                <div class="main">
+                    <div class="xp-and-level-box">
+                        <div class="level-box">
+                            <h2>Level 24</h2>
+                        </div>
+                        <div class="xp-box">
+                            <h2>597 Kb</h2>
+                        </div>
+                    </div>
+                    <div class="audit-ratio-box">
+                        <h3>audits info</h3>
+                        <div class="audit-details">
+                            <div class="stats done"></div>
+                            <div class="stats received"></div>
+                        </div>
+                        <div class="audit-main">
+                            <div class="audit-ratio"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <div class="skills-modules-wrapper">
+            <div class="skills-section">
+                <h3 style="font-weight: 800;">skills</h3>
+            </div>
+            <div class="module-section">
+                <h3 style="font-weight: 800;">module</h3>
+            </div>
+        </div>
+        </div>
+    </main>
+`
+
+export {Login, Profile}
